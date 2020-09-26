e77fe3f879d0cfa89daf0a1404e480b64bb03d00
RomainLefranc <lefranc46@gmail.com>
2020-09-26 20:40:51 +0400

    v3.0

---
+++ modele/pdo.php
@@ -0,0 +1,43 @@
+<?php
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+    } catch (Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+
+
+    /*  Accueil Trombi
+
+            SELECT COUNT(*) 
+                FROM tableStagiaires
+            GROUP BY LEFT(NomStagiaire,1),COUNT(*)
+            ORDER BY LEFT(NomStagiaire,1) ASC
+
+            SELECT *
+                FROM tableSections 
+            ORDER BY codeSection ASC
+
+        Choix 1 initiale
+            
+            SELECT codSec, codSta, nomSta, preSta, interneSta
+                FROM tableStagiaires
+                    WHERE LEFT(nomSta,1) = $variable    
+            ORDER BY nomSta,preSta ASC
+
+        Clic sur 1 stagiaire
+
+            SELECT *
+                FROM tableStagiaires
+                WHERE codSec = $_GET['section'] AND codSta = $_GET['stagiaire']
+
+        Choix 1 section
+
+        SELECT * 
+            FROM tableStagiaires
+            WHERE codSec = $_GET['section']
+        ORDER BY nomSta ASC
+    */
+
+
+
+?>
