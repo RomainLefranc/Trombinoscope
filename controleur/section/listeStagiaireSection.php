0dd2439b2d1d5eb3e5310bec41dab182b56c8a6b
RomainLefranc <lefranc46@gmail.com>
2020-09-27 18:36:56 +0400

    v3.0

--- controleur/section/listeStagiaireSection.php
+++ controleur/section/listeStagiaireSection.php
@@ -4,13 +4,13 @@
     foreach ($donneeStagiaire as $stagiaire) {
         if ($codeSection == $stagiaire[0]) {
             $donnee.= '
-            <tr>
-                <th class="align-middle text-center" scope="col">'.$stagiaire[1].'</th>
-                <th class="align-middle text-center" scope="col">'.$stagiaire[2].'</th>
-                <th class="align-middle text-center" scope="col">'.$stagiaire[3].'</th>
-                <th class="align-middle text-center" scope="col">'.$stagiaire[6].'</th>
-                <th class="align-middle text-center" scope="col"><img class="img-fluid"src="https://via.placeholder.com/100x100.png?text='.$stagiaire[2].'" alt="">
-                <th class="align-middle text-center" scope="col"><a class="btn btn-primary" href="fiche.php?stagiaire='.$stagiaire[1].'&section='.$stagiaire[0].'" role="button"><i class="fas fa-eye"></i></a></th>
+            <tr class="text-center">
+                <th class="align-middle">'.$stagiaire[1].'</th>
+                <th class="align-middle">'.$stagiaire[2].'</th>
+                <th class="align-middle">'.$stagiaire[3].'</th>
+                <th class="align-middle">'.$stagiaire[6].'</th>
+                <th class="align-middle"><img class="img-fluid"src="https://via.placeholder.com/100x100.png?text='.$stagiaire[2].'" alt="">
+                <th class="align-middle"><a class="btn btn-primary" href="fiche.php?stagiaire='.$stagiaire[1].'&section='.$stagiaire[0].'" role="button"><i class="fas fa-eye"></i></a></th>
             </tr>
             ';
         }
