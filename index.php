2d65d489b54ee69ec6aa328c7c4c37a4272b5a1e
DEVLOG.E09 <DEVLOG.E09@afpar.com>
2020-10-06 16:01:56 +0400

    v10.5

--- index.php
+++ index.php
@@ -1,35 +1,7 @@
 <?php
-/* include "modele/data.php"; */
-/* if (isset($_GET["action"])) {
-    switch ($_GET["action"]) {
-        case "A":
-            include "controleur/C_accueil.php";
-            break;
-        case "T":
-            include "controleur/C_trombi.php";
-            break;
-        case 'S':
-            include "controleur/C_section.php"
-;            break;
-        case 'I':
-            include "controleur/C_initiale.php";
-            break;
-        case 'F':
-            include "controleur/C_fiche.php";
-            break;
-        case 'D':
-            include "controleur/C_deconnexion.php";
-            break;
-        default:
-            include "view/404.php";
-            break;
-    }
-} else {
-    header("location: index.php?action=A");
-} */
+
 session_start();
-include "modele/data.php";
-include "controleur/controleur.php";
+include "controleur/controleur.php";/* Outil permettant la verification des parametre reçus */
 $pages = array (["A","accueil"],["T","trombi"],["S","section"],["I","initiale"],["F","fiche"],["D","deconnexion"],["L","gestion"],["C","cru"]);
 $controlExiste = false;
 if (verifAction()) {
